526571dd5f8455437651adf38373c439a8d9a3f2
Camilo Agudelo <jcagudelo12@gmail.com>
2021-03-21 19:49:12 -0500

    Add icons to principal menu

--- navigations/Navigation.jsx
+++ navigations/Navigation.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Icon } from "react-native-elements";
+
 import SalesStack from "./SalesStack";
 import TransmissionsStack from "./TransmissionsStack";
 import AccountStack from "./AccountStack";
@@ -8,9 +10,35 @@ import AccountStack from "./AccountStack";
 const Tab = createBottomTabNavigator();
 
 export default function Navigation() {
+  const screenOptions = (route, color) => {
+    let iconName;
+    switch (route.name) {
+      case "sales":
+        iconName = "cart-arrow-down";
+        break;
+      case "transmissions":
+        iconName = "cube-send";
+        break;
+      case "account":
+        iconName = "account-circle-outline";
+        break;
+    }
+    return (
+      <Icon type="material-community" name={iconName} size={30} color={color} />
+    );
+  };
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        initialRouteName="account"
+        tabBarOptions={{
+          inactiveTintColor: "#4A4A4A",
+          activeTintColor: "#CCDB33",
+        }}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color }) => screenOptions(route, color),
+        })}
+      >
         <Tab.Screen
           name="sales"
           component={SalesStack}
